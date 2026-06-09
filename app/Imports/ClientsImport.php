42e190938c3da64137dce69ec690e6bae8a82a06
Harshna-Patel <harshnapatel8121998@gmail.com>
2026-06-09 00:23:34 -0700

    fix client import issue

--- app/Imports/ClientsImport.php
+++ app/Imports/ClientsImport.php
@@ -17,71 +17,47 @@ class ClientsImport implements ToModel, WithHeadingRow
     {
         if(isset($row['is_shipping_address_same']) && $row['is_shipping_address_same']) {
             $add_shipping_address = 'same_as_billing';
-            $shipping_street_address_1 = $this->rowValue($row, ['billing_street_address_1', 'client_street_address_1']);
-            $shipping_city = $this->rowValue($row, ['billing_city', 'client_city']);
-            $shipping_state = $this->rowValue($row, ['billing_state', 'client_state']);
-            $shipping_postalcode = $this->rowValue($row, ['billing_postalcode', 'client_postalcode']);
+            $shipping_street_address_1 = $row['billing_street_address_1'];
+            $shipping_city = $row['billing_city'];
+            $shipping_state = $row['billing_state'];
+            $shipping_postalcode = $row['billing_postalcode'];
         }else {
             $add_shipping_address = 'shipping_address_diff';
-            $shipping_street_address_1 = $this->rowValue($row, ['shipping_street_address_1']);
-            $shipping_city = $this->rowValue($row, ['shipping_city']);
-            $shipping_state = $this->rowValue($row, ['shipping_state']);
-            $shipping_postalcode = $this->rowValue($row, ['shipping_postalcode']);
+            $shipping_street_address_1 = $row['shipping_street_address_1'];
+            $shipping_city = $row['shipping_city'];
+            $shipping_state = $row['shipping_state'];
+            $shipping_postalcode = $row['shipping_postalcode'];
         }
         
-        $customer_number = $this->rowValue($row, ['customernumber', 'client_number']);
-        if(!$customer_number) {
+        if(isset($row['customernumber']) && $row['customernumber']) {
+            $customer_number = $row['customernumber'];
+        }else {
             $customer_number = null;
         }
 
-        $reminder_day = $this->rowValue($row, ['reminder_days', 'reminder_day']);
-        if(!$reminder_day) {
-            $reminder_day = 15;
-        }
-
-        $client_notes = $this->rowValue($row, ['notes', 'client_notes']);
-        $client_tags = $this->rowValue($row, ['tags', 'client_tags']);
-        if(!$client_tags) {
-            $client_tags = null;
-        }
-
-        $client_currency = $this->rowValue($row, ['client_currency']) ?: null;
-
         return new Client([
             'client_number' => $customer_number,
-            'client_business_name' => $this->rowValue($row, ['clientbusinessname', 'client_business_name']), 
-            'client_first_name' => $this->rowValue($row, ['firstname', 'client_first_name']), 
-            'client_last_name' => $this->rowValue($row, ['lastname', 'client_last_name']), 
-            'client_mobile' => $this->rowValue($row, ['mobile', 'client_mobile']), 
-            'client_street_address_1' => $this->rowValue($row, ['billing_street_address_1', 'client_street_address_1']),
-            'client_city' => $this->rowValue($row, ['billing_city', 'client_city']), 
-            'client_state' => $this->rowValue($row, ['billing_state', 'client_state']), 
-            'client_postalcode' => $this->rowValue($row, ['billing_postalcode', 'client_postalcode']), 
+            'client_business_name' => $row['clientbusinessname'], 
+            'client_first_name' => $row['firstname'], 
+            'client_last_name' => $row['lastname'], 
+            'client_mobile' => $row['mobile'], 
+            'client_street_address_1' => $row['billing_street_address_1'],
+            'client_city' => $row['billing_city'], 
+            'client_state' => $row['billing_state'], 
+            'client_postalcode' => $row['billing_postalcode'], 
             'add_shipping_address' => $add_shipping_address, 
             'shipping_street_address_1' => $shipping_street_address_1, 
             'shipping_city' => $shipping_city,
             'shipping_state' => $shipping_state, 
             'shipping_postalcode' => $shipping_postalcode, 
-            'client_email' => $this->rowValue($row, ['accounts_email', 'invoice_email', 'client_email']), 
-            'client_quotes_email' => $this->rowValue($row, ['quote_email', 'quotes_email', 'client_quotes_email']),
-            'client_statement_email' => $this->rowValue($row, ['statement_email', 'client_statement_email']),
-            'reminder_day' => $reminder_day,
-            'client_notes' => $client_notes,
-            'client_tags' => $client_tags,
+            'client_email' => $row['accounts_email'], 
+            'client_quotes_email' => $row['quotes_email'], 
+            'client_statement_email' => $row['statement_email'],
+            'reminder_day' => $row['reminder_days'] ?? 15, 
+            'client_notes' => $row['notes'],
+            'client_tags' => $row['tags'],
             'client_invoicing_method' => 'send_via_email',
-            'client_currency' => $client_currency,
             'is_status' => 1
         ]);
-    }
-
-    private function rowValue(array $row, array $keys)
-    {
-        foreach ($keys as $key) {
-            if (isset($row[$key]) && $row[$key] !== '') {
-                return $row[$key];
-            }
-        }
-
-        return null;
     }
 }
